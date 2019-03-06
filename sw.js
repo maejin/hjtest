8d5633fe3661d7f9d7a2ad978067fc644cc530b9
maejin <heehye85@gmail.com>
2019-03-06 14:53:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
 ﻿
+var zero_cache = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(zero_cache)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
