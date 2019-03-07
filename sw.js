c9f4f8555980f9a729495322ef04e2ea69a6dd2b
maejin <heehye85@gmail.com>
2019-03-07 17:39:53 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,6 +35,7 @@ console.log( response );
 
 self.addEventListener('push', function(event) {
   //푸시 리스너
+  console.log( evenet );
 	var payload = event.data.json();
 	const title = payload.title;
 	const options = {
