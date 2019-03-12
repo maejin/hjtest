bac0cb194c044251e88a63abeaf7a99d81cdbfd3
maejin <heehye85@gmail.com>
2019-03-12 16:44:31 +0900

    Update generator_base.js

--- generator_base.js
+++ generator_base.js
@@ -1,4 +1,4 @@
-﻿// Copyright 2015 Peter Beverloo. All rights reserved.
+// Copyright 2015 Peter Beverloo. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
